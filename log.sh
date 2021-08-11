fdbdc6c0ec8b69a4ecd2d35b71f75413c0c3c43a
Eric Harding <eric@digitalsorcery.net>
2021-08-11 15:03:56 -0400

    log

---
+++ log.sh
@@ -0,0 +1,5 @@
+log() {
+	level=$1
+	shift
+	echo [$(date "+%Y-%m-%d %H:%m:%S")] [$level] $@ >> ~/aact.log
+}
--- update_aact.sh
+++ update_aact.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+source log.sh
 
 # Download the latest snapshot
+log info Downloading AACT snapshot
 ./download_aact_snapshot.sh
 
+log info Download complete. Validating...
 # Check if the snapshot downloaded successfully
 if zipinfo clinical_trials.zip > /dev/null; then
-   echo Valid zip file
+   log info Valid zip file
 else
-   echo Invalid zip. Abort. | tee log.txt
+   log error Invalid zip. Abort.
    exit
 fi
 
 # unzip into temp
+log info Decompressing...
 WORK=snapshot_temp
 rm -rf $WORK
 mkdir -p $WORK
@@ -21,8 +25,10 @@ unzip clinical_trials.zip -d $WORK
 
 source .env
 
+log info Applying snapshot...
 docker run --rm -it -v $PWD:/work -w /work postgres:$PGVERSION ./apply_snapshot.sh $WORK/postgres_data.dmp $PGHOST $PGPORT $PGUSER $PGDATABASE $PGPASS $FINAL_SCHEMA
 
 rm -rf $WORK
+log info Loading metadata...
 ./reload_hasura_metadata.sh $HASURA_SECRET $HASURA_HOST
 
