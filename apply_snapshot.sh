b166249a1e17ffe33c7f2386036b9f05dc741703
Eric Harding <eric@digitalsorcery.net>
2021-08-13 13:45:48 -0400

    remove redundant logs

--- apply_snapshot.sh
+++ apply_snapshot.sh
@@ -35,7 +35,5 @@ set -e # exit on error so if pg_restore fails we don\'t drop ctgov_prod
 pg_restore --exit-on-error -v --no-owner --no-acl -h $PGHOST -U $PGUSER -p $PGPORT -w -d $PGDATABASE -n ctgov $FILENAME
 log info pg_restore success!
 run_sql "DROP SCHEMA IF EXISTS ctgov_old CASCADE; ALTER SCHEMA $FINAL_SCHEMA RENAME TO ctgov_old;"
-log info renamed $FINAL_SCHEMA to ctgov_old
 run_sql "DROP SCHEMA IF EXISTS $FINAL_SCHEMA CASCADE; ALTER SCHEMA ctgov RENAME TO $FINAL_SCHEMA;"
-log info renamed ctgov to $FINAL_SCHEMA
 
