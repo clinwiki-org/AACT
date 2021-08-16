19ae2d9c72c218b7205335d7f5d58ef0999f5b07
Eric Harding <eric@digitalsorcery.net>
2021-08-16 08:59:57 -0400

    double quotes allow variable expansion

--- reload_hasura_metadata.sh
+++ reload_hasura_metadata.sh
@@ -3,6 +3,6 @@
 HASURA_SECRET=$1
 HASURA_HOST=$2
 
-curl -H 'X-Hasura-Admin-Secret: $HASURA_SECRET' -d'{"type":
+curl -H "X-Hasura-Admin-Secret: $HASURA_SECRET" -d'{"type":
 "reload_metadata", "args": {}}' https://$HASURA_HOST/v1/metadata
 
